539b01d25d2c7b5b6dffa13552861455cad04754
Aditya <adityaas26@gmail.com>
2019-04-30 09:55:19 +0530

    initial commit

---
+++ The MAD Corp new/the-mad-corp/gridsome.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteName: 'Gridsome',
+  siteDescription: 'A WordPress starter for Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'https://thelonelyauthorblog.com/', // required
+        typeName: 'WordPress', // GraphQL schema name (Optional)
+        perPage: 3, // How many posts to load from server per request (Optional)
+        concurrent: 10, // How many requests to run simultaneously (Optional)
+        routes: {
+          post: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
+          post_tag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+        }
+      }
+    }
+  ]
+}
